26cc4c1f0f5532e74eca5c19ee3e8788a7fb6f55
konrepo <37514522+konrepo@users.noreply.github.com>
2026-03-16 22:52:23 -0400

    Add files via upload

---
+++ src/manifest.js
@@ -0,0 +1,32 @@
+const TYPE = "series";
+const EXTRA = ["search", "skip"];
+
+const sites = [
+  { id: "vip", name: "Phumikhmer" },
+  { id: "sunday", name: "SundayDrama" },
+  { id: "idrama", name: "iDramaHD" },
+  { id: "khmerave", name: "KhmerAve" },
+  { id: "merlkon", name: "Merlkon" },
+];
+
+module.exports = {
+  id: "community.khmer.test",
+  version: "1.3.0-test",
+  name: "KhmerDub Test",
+  description: "Stream Experimental Build | Dev: TheDevilz.",
+  logo: "https://raw.githubusercontent.com/konrepo/VIP/refs/heads/main/test.png",
+
+  resources: ["catalog", "meta", "stream"],
+  types: [TYPE],
+
+  catalogs: sites.map(site => ({
+    type: TYPE,
+    id: site.id,
+    name: `${site.name} (test)`,
+    extraSupported: EXTRA
+  })),
+
+  behaviorHints: {
+    configurable: false
+  }
+};
